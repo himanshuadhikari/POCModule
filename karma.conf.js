e209fefba7c959914d719d1164ea880ca55ff674
himanshu adhikari <himanshu.adhikari@tothenew.com>
2017-02-07 17:07:47 +0530

    changed directory structure

--- karma.conf.js
+++ karma.conf.js
@@ -18,7 +18,7 @@ module.exports = function(config) {
             'bower_components/jquery/dist/jquery.js',
             'bower_components/angular/angular.js',
             'bower_components/angular-mocks/angular-mocks.js',
-            'src/**/*.js',
+            'src/scripts/**/*.js',
             'test/**/*.js'
         ],
 
